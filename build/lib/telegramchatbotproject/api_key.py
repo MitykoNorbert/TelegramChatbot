5de217d81f21ce104c957f3ad3cc525b9a7d430a
MitykoNorbert <100774291+MitykoNorbert@users.noreply.github.com>
2023-04-30 18:24:42 +0200

    szükségtelen build fileok törlése

--- build/lib/telegramchatbotproject/api_key.py
+++
@@ -1,6 +0,0 @@
-"""
-Ez a modul tartalmazza a telegram bot működéséhez szükséges
-API Kulcsot, amit a telegram rendszeréből kapunk a bot
-létrehozásakor.
-"""
-API_KEY = '6154049059:AAHlqaL1PV4qeoX8iudz-5CfzsHce4CunN8'
