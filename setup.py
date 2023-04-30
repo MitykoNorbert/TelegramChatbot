c18ec2f27cef02ef557ca466d4fb9907cef409a9
MitykoNorbert <norbert.mityko001@gmail.com>
2023-04-30 19:25:34 +0200

    build changes update on filepaths

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='TelegramChatbotProject',
-    version='1.0.0',
+    version='1.0.1',
     packages=['telegramchatbotproject'],
     url='https://github.com/MitykoNorbert/TelegramChatbot',
     license='',
