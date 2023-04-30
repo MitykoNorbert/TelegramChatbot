83ff0b7e6c1586a8f04517bce6e820e2ab954cce
MitykoNorbert <norbert.mityko001@gmail.com>
2023-04-30 18:30:26 +0200

    filepath updated on panasz.py

--- TelegramChatbotProject/panasz.py
+++ TelegramChatbotProject/panasz.py
@@ -25,7 +25,7 @@ class Panasz:
         """
         A panaszt file-ba mentő függvény
         """
-        filename = f"Reklamaciok/{self.username}-{self.ido.strftime('%Y-%m-%d_%H-%M-%S')}.txt"
+        filename = f"TelegramChatbotProject/Reklamaciok/{self.username}-{self.ido.strftime('%Y-%m-%d_%H-%M-%S')}.txt"
         with open(filename, "w", encoding="utf-8") as file:
             file.write(f"{str(self.username)}\n{self.text}\n")
 
